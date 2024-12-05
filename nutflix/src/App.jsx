e54a6a31820737177877f718fc599f655c856b69
Juan José Angarita Yela <129020936+RamonaFlores@users.noreply.github.com>
2024-12-04 22:52:47 -0500

    Project start! Vite react app created + initial dependency configuration

---
+++ nutflix/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import CssBaseline from '@mui/material/CssBaseline';
+import {BrowserRouter as Router , Routes , Route} from 'react-router-dom'
+function App() {
+  return (
+    <div>
+      <CssBaseline/>
+      <h1> Hello, world </h1>
+    </div>
+  )
+}
+
+export default App

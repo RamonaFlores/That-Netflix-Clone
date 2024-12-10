9cb02791d66d892612eb0db2f72c12be53a18389
Juan José Angarita Yela <129020936+RamonaFlores@users.noreply.github.com>
2024-12-09 21:22:23 -0500

    feat: First advancements

--- nutflix/src/App.jsx
+++ nutflix/src/App.jsx
@@ -1,12 +1,37 @@
 import React from 'react'
 import CssBaseline from '@mui/material/CssBaseline';
-import {BrowserRouter as Router , Routes , Route} from 'react-router-dom'
+import { Routes , Route,BrowserRouter as Router} from 'react-router-dom'
+import {Actors,MovieInformation,Movies,NavBar,Profile} from './index.js'
+
+import useStyles from './styles.js'
+function AppContent() {
+  const classes= useStyles()
+  return (
+    <div className={classes.root}>
+      <CssBaseline/>
+      <NavBar/>
+      <main className={classes.content}>
+      <div className={classes.toolbar}>
+
+      
+            <Routes>
+              <Route path="/" element={<Movies />} />
+              <Route path="/movie/:id" element={<MovieInformation />} />
+              <Route path="/actors/:id" element={<Actors />} />
+              <Route path="/profile/:id" element={<Profile />} />
+            </Routes>
+        </div>
+      </main>
+      
+    </div>
+    
+  )
+}
 function App() {
   return (
-    <div>
-      <CssBaseline/>
-      <h1> Hello, world </h1>
-    </div>
+    <Router>
+      <AppContent/>
+    </Router>
   )
 }
 
---
+++ nutflix/src/components/NavBar/NavBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { AppBar,IconButton, Toolbar, Drawer, Button, Avatar } from '@mui/material'
+import {Menu, AccountCircle, Brigthness4,Brigthness7} from '@mui/icons-material'
+import {Link} from 'react-router-dom'
+const NavBar = () => {
+  return (
+    <div>NavBar</div>
+  )
+}
+
+export default NavBar
